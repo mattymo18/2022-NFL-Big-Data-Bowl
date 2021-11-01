76885a6c90d19654d9b2e4c2e2ba7942f83eb45a
MATT JOHNSON <johnson.matt1818@gmail.com>
2021-11-01 18:45:44 -0400

    added build script for sparse matrix used in regression

---
+++ Scripts/Player.Matrix.Cleaner.R
@@ -0,0 +1,72 @@
+#load libs
+library(tidyverse)
+library(Matrix)
+#we can start with the play data, we can take out all the punts here
+plays <- read.csv("Source_Data/plays.csv")
+
+player.tracker <- read_csv("Derived_Data/Player.Tracking.csv")
+
+games <- read_csv("Source_Data/games.csv")
+
+punts <- plays %>% 
+  filter(specialTeamsPlayType == "Punt") %>% 
+  mutate(NetYardsGained = ifelse(specialTeamsResult == "Return", kickReturnYardage, 0)) %>% 
+  #we also dont want any muffs or fumbles, we only want when he catches it or doesn't try to catch it
+  filter(specialTeamsResult == "Return" | specialTeamsResult == "Touchback" | 
+           specialTeamsResult == "Fair Catch" | specialTeamsResult == "Downed") %>% 
+  #make our newId
+  unite(newId, c(gameId, playId), remove = F)
+
+#ok now this is simple, I can just join and mutate
+############################################# we need to fix starting here, rerun Tracking.Cleaner.R ###############3
+player.tracker.temp1 <- left_join(player.tracker, games, by = "gameId")
+
+#now we can mutate and clean up a bit
+
+player.tracker.temp2 <- player.tracker.temp1 %>% 
+  mutate(Team = ifelse(Side == "home", homeTeamAbbr, visitorTeamAbbr)) %>% 
+  select(newId, nflId, Name, Side, Team)
+
+#great now we can figure out who punted and who returned
+
+
+#we can do the same join
+player.tracker.temp3 <- left_join(player.tracker.temp2, punts, by = "newId")
+
+
+#and do the same mutate and clean up
+
+player.tracker.temp4 <- player.tracker.temp3 %>% 
+  mutate(ReturnTeam = ifelse(Team == possessionTeam, -1, 1)) %>% 
+  select(newId, nflId, Name, Side, Team, ReturnTeam)
+
+#now we can build a sparse matrix by finding all the indices for where the nonzero entries are
+
+plays.indx <- tibble(
+  Index = 1:length(unique(player.tracker.temp4$newId)), 
+  Play = unique(player.tracker.temp4$newId)
+)
+
+players.indx <- tibble(
+  Index = 1:length(unique(player.tracker.temp4$nflId)), 
+  Player = unique(player.tracker.temp4$nflId)
+)
+
+#then we can use match to place them into the dataframe
+
+player.tracker.temp4$RowIdx <- match(player.tracker.temp4$newId, plays.indx$Play)
+player.tracker.temp4$ColIdx <- match(player.tracker.temp4$nflId, players.indx$Player)
+
+#finally we can use the sparseMatrix function and simply specify where the nonzero entries should be
+
+sparse.mat <- sparseMatrix(
+  i = player.tracker.temp4$RowIdx, 
+  j = player.tracker.temp4$ColIdx, 
+  x = player.tracker.temp4$ReturnTeam,
+  dims = c(nrow(plays.indx), nrow(players.indx)), 
+  dimnames = list(plays.indx$Play, players.indx$Player)
+)
+
+#now we don't want to make this a dense matrix and save it, so we'll need to write the matrix in a special way
+
+writeMM(sparse.mat, "Derived_Data/Sparse.Matrix.txt")
